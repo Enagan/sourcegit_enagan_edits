3a256a34a99bb5dd7a1ffdf33a74147689b84f5d
Enagan <pedro.a.f.engana@gmail.com>
2025-06-30 12:50:16 +0100

    Attempt to also redirect stdinput to avoid gitp4 crashes

--- src/ViewModels/ExecuteCustomAction.cs
+++ src/ViewModels/ExecuteCustomAction.cs
@@ -216,9 +216,11 @@ namespace SourceGit.ViewModels
             start.UseShellExecute = false;
             start.CreateNoWindow = true;
             start.RedirectStandardOutput = true;
+            start.RedirectStandardInput = true;
             start.RedirectStandardError = true;
             start.StandardOutputEncoding = Encoding.UTF8;
             start.StandardErrorEncoding = Encoding.UTF8;
+            start.StandardInputEncoding = Encoding.UTF8;
             start.WorkingDirectory = _repo.FullPath;
 
             var proc = new Process() { StartInfo = start };
